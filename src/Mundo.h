0e05f6df1077f45ef9fd34b83205bc1f1bbc6c60
Sergio Rodríguez Sanz <80534752+Srsanz@users.noreply.github.com>
2023-05-12 18:42:34 +0200

    Create Mundo.h

---
+++ src/Mundo.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Tablero.h"
+class Mundo
+{
+public:
+
+	Tablero tablero;
+
+	void tecla(unsigned char key);
+	void inicializa();
+	void mueve();
+	void dibuja();
+
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+};
