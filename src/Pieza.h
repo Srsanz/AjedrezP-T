1491c336a7a64666efd8bf34c2d8f0d793cf870f
laurasedanomolero <80411618+laurasedanomolero@users.noreply.github.com>
2023-05-16 19:22:04 +0200

    Create Pieza.h

---
+++ src/Pieza.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+enum class TipoPieza {
+    None,
+    Caballo,
+    Peon
+};
+
+
+class Pieza
+{
+public:
+    TipoPieza tipo;
+    int x, y;
+    bool is_white = 0;
+
+    Pieza() : tipo(TipoPieza::None), x(0), y(0) {};
+
+    virtual void dibuja() const {};
+
+};
+
+
+
