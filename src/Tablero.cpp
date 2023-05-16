04306aad86fd89a9a99c7b92f1fc0cd17b479d8b
Natalia <natalia.miguel@alumnos.upm.es>
2023-05-16 19:16:37 +0200

    Create Tablero.cpp

---
+++ src/Tablero.cpp
@@ -0,0 +1,99 @@
+#include "Tablero.h"
+#include "Peon.h"
+
+
+Tablero::~Tablero()
+{
+	//pieza.destruirContenido();
+
+	// Liberar memoria de las piezas
+	/*
+	for (int y = 0; y < 8; y++) {
+		for (int x = 0; x < 8; x++) {
+			delete board[x][y];
+		}
+	}
+	*/
+}
+
+
+
+void Tablero::dibuja() {
+
+	gluLookAt(x_ojo, y_ojo, z_ojo, // posicion del ojo
+		x_ojo, y_ojo, 0.0, // hacia que punto mira (0,0,0)
+		0.0, 1.0, 0.0); // definimos hacia arriba (eje Y)
+
+
+	// dibujo del tablero
+	for (int y = 0; y < 8; y++) {
+		for (int x = 0; x < 8; x++) {
+			if ((x + y) % 2 == 0) {
+				glColor3f(1.0, 1.0, 1.0); // Color blanco
+			}
+			else {
+				glColor3f(0.7, 0.7, 0.7); // Color gris claro
+			}
+			glBegin(GL_QUADS);
+			glVertex2i(x * cellSize, y * cellSize);
+			glVertex2i((x + 1) * cellSize, y * cellSize);
+			glVertex2i((x + 1) * cellSize, (y + 1) * cellSize);
+			glVertex2i(x * cellSize, (y + 1) * cellSize);
+			glEnd();
+
+			// Dibujar la pieza en la casilla
+			if (board[x][y] != nullptr) {
+				glPushMatrix();
+				glTranslatef(x * cellSize, y * cellSize, 0.1);
+				board[x][y]->dibuja();
+				glPopMatrix();
+			}
+		}
+	}
+
+
+
+}
+
+void Tablero::inicializa() {
+
+
+	x_ojo = 20;
+	y_ojo = 20;
+	z_ojo = 30; // habria q mirarlo bien
+
+
+	// Inicializacion del tablero	
+	for (int y = 0; y < 8; y++) {
+		for (int x = 0; x < 8; x++) {
+			board[x][y] = nullptr;
+		}
+	}
+
+
+
+	// Inicializacion de las piezas
+	/*
+	for (int i = 0; i < 8; i++)
+	{
+		board[i][1] = new Peon(i, 1);
+	}
+	*/
+	//board[4][5] = new Peon(5,6);
+	//board[0][6] = new Peon(2, 6);
+	board[0][1] = new Peon(0, 1);
+	board[1][6] = new Peon(1, 6);
+
+}
+
+void Tablero::tecla(unsigned char key) {
+
+
+
+}
+
+void Tablero::mueve() {
+
+
+
+}
