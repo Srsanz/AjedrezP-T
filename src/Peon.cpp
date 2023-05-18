69c6b13056086aa47405f9a9e9eee44eaa3bac44
laurasedanomolero <80411618+laurasedanomolero@users.noreply.github.com>
2023-05-18 16:43:48 +0200

    Update Peon.cpp

--- src/Peon.cpp
+++ src/Peon.cpp
@@ -1,19 +1,21 @@
 #include "Peon.h"
 #include "freeglut.h"
-#include <iostream>
+
+void Peon:: dibuja() {
+
+	glPushMatrix();
+	glColor3f(1.0f, 0.0f, 0.0f);
+	if(numero<=8){
+		sprite1.draw();
+	}
+	if (numero > 8) {
+		sprite2.draw();
+	}
+	glPopMatrix();
+}
 
 
-/*
-void Peon::dibuja() const {
-	glColor3f(1.0, 1.0, 0.0); // Color amarillo
-	glBegin(GL_QUADS);
-	glVertex2i(x * cellSize, y * cellSize);
-	glVertex2i((x + 1) * cellSize, y * cellSize);
-	glVertex2i((x + 1) * cellSize, (y + 1) * cellSize);
-	glVertex2i(x * cellSize, (y + 1) * cellSize);
-	glEnd();
 
-}*/
 
 /*
 void Peon::setPos(float ix, float iy)
