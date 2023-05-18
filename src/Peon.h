fe5412b9f35ca53fe5f621a01d2df7d6c83a7e11
laurasedanomolero <80411618+laurasedanomolero@users.noreply.github.com>
2023-05-18 16:43:59 +0200

    Update Peon.h

--- src/Peon.h
+++ src/Peon.h
@@ -8,38 +8,35 @@ using ETSIDI::SpriteSequence;
 const int piezeSize = 1;
 const int cellSize = 5;
 
+
+
 class Peon : public Pieza
 {
-
-	SpriteSequence sprite1{ "imagenes/peonnegro.png", 1 };
-	SpriteSequence sprite2{ "imagenes/peonblanco.png", 1 };
+	SpriteSequence sprite1{ "imagenes/peonblanco.png", 1 };
+	SpriteSequence sprite2{ "imagenes/peonnegro.png", 1 };
 	//int x, y;
+	int numero;
 
 public:
 	// AÑADIR VECTOR 2D
 	// Vector2D posicion;
 
 	Peon(int num) {
+		numero = num;
 		tipo = TipoPieza::Peon;
-
+		sprite1.setCenter(-0.5, -0.5);
+		sprite1.setSize(4, 4);
+		sprite2.setCenter(-0.5, -0.5);
+		sprite2.setSize(4, 4);
+		altura = 1.8f;
 	}
-	//virtual~Peon(){};
 
-	//void dibuja() const override ;
+	virtual~Peon(){};
 
+	float altura;
 
-	void dibuja() const override {
+	void dibuja() override ;
 
-		glColor3f(1.0, 0.0, 0.0); // Color amarillo
-		glBegin(GL_QUADS);
-		glVertex2i(0, 0);
-		glVertex2i(cellSize, 0);
-		glVertex2i(cellSize, cellSize);
-		glVertex2i(0, cellSize);
-		glEnd();
-
-
-	}
 
 	// FUNCIONES DEL PEON
 	/*
@@ -49,4 +46,5 @@ public:
 	void mover();
 
 	*/
+
 };
