a6c366993586fea2180a68c5f7633baee356df14
Antonio García Montes <antonio.gmontes@alumnos.upm.es>
2023-05-19 10:55:15 +0200

    Update Movimientos.cpp

--- src/Movimientos.cpp
+++ src/Movimientos.cpp
@@ -1,1 +1,14 @@
+#pragma once
 
+#include "Movimientos.h"
+#include "math.h"
+
+class Movimientos {
+	int x_ini, y_ini;
+	int x_fin, y_fin;
+
+public:
+
+	Movimiento(int x, int y): x_ini(x), 
+	bool MovimientoPeon()
+};
