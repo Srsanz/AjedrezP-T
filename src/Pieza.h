63088899e71e54bb3cf72f50d9eefed048435146
Natalia <natalia.miguel@alumnos.upm.es>
2023-05-21 11:58:29 +0200

    Update Pieza.h

--- src/Pieza.h
+++ src/Pieza.h
@@ -14,11 +14,12 @@ public:
     TipoPieza tipo;
     int x, y;
     bool is_white = 0;
-
+    int numero;
     Pieza() : tipo(TipoPieza::None), x(0), y(0) {};
 
     virtual void dibuja() {};
    virtual void mover() {};
+   int obtenerNumero() const;
 };
 
 
