91f9c119d75b415a3f50a460ea0f0a61b3b89a68
Avalcarcelglez <alberto.valcarcel.gonzalez@alumnos.upm.es>
2023-05-21 18:11:28 +0200

    Caballo

---
+++ src/Caballo.cpp
@@ -0,0 +1,15 @@
+#include "freeglut.h"
+#include "Caballo.h"
+
+void Caballo::dibuja() {
+
+	glPushMatrix();
+	glColor3f(1.0f, 0.0f, 0.0f);
+	if (color == 'b') {
+		sprite1.draw(); //blancas
+	}
+	else if (color == 'n') {
+		sprite2.draw(); //negras
+	}
+	glPopMatrix();
+}
