470111d48e2c6f2e4f1d781b03d818a7b4ef6a6b
laurasedanomolero <80411618+laurasedanomolero@users.noreply.github.com>
2023-05-21 17:55:42 +0200

    Update Peon.cpp

--- src/Peon.cpp
+++ src/Peon.cpp
@@ -15,9 +15,43 @@ void Peon:: dibuja() {
 }
 
 
-void Peon::mover() {
-	
+bool Peon::mover(int xInicial, int yInicial, int xFinal, int yFinal) {
 
+	// Verificar si el movimiento es válido para un peón
+	//bool movimientoValido = false;
+
+	// Si el peón es blanco
+	if (color == 'b') {
+		// Movimiento válido: mover hacia adelante una casilla
+		if (yFinal == yInicial + 1 && xFinal == xInicial) {
+			return true;
+		}
+		// Movimiento válido: mover hacia adelante dos casillas desde la posición inicial
+		else if (yFinal == yInicial + 2 && xFinal == xInicial && yInicial == 1) {
+			return true;
+		}
+		// Movimiento válido: captura diagonal a la derecha
+		else if (yFinal == yInicial + 1 && (xFinal == xInicial + 1 || xFinal == xInicial - 1)) {
+			return true;
+		}
+	}
+	// Si el peón es negro
+	else if (color == 'n') {
+		// Movimiento válido: mover hacia adelante una casilla
+		if (yFinal == yInicial - 1 && xFinal == xInicial) {
+			return true;
+		}
+		// Movimiento válido: mover hacia adelante dos casillas desde la posición inicial
+		else if (yFinal == yInicial - 2 && xFinal == xInicial && yInicial == 6) {
+			return true;
+		}
+		// Movimiento válido: captura diagonal a la izquierda
+		else if (yFinal == yInicial - 1 && (xFinal == xInicial + 1 || xFinal == xInicial - 1)) {
+			return true;
+		}
+	}
+	
+	return false;
 }
 
 
